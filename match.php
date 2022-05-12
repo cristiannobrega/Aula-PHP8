ebff3eca97c6f274438a4cbcc6b435bb5680bd85
Cristian Nobrega <cristian.nobrega@gmail.com>
2022-05-12 12:36:54 -0300

    Acrescentado mais algumas definições de switch e match.

--- match.php
+++ match.php
@@ -36,8 +36,17 @@ echo "</pre>";
 /*
 O switch é uma declaração e match é uma expressão. Portanto, switch por si só não é capaz de 
 produzir um valor ao ser avaliado. Já match, sim.
-Ausência de coerção de tipos. Ao comparar os valores de cada case, 
-switch utiliza o operador == (que faz conversão automática de tipos). 
+Ausência de coerção de tipos. 
+Ao comparar os valores de cada case,switch utiliza o 
+operador == (que faz conversão automática de tipos). 
 Já o match, ao fazer as avaliações, utiliza === (que não faz coerção de tipos).
+*
+o match é uma expressão e, por isso, sempre deve retornar um valor. 
+Por isso, o uso de match será agora recomendado quando tiver que se retornar um valor.
+*
+A finalidade do switch não é retornar resultados, e sim controlar fluxo. 
+Nas linguagens onde é implementado corretamente, o switch equivale ao goto
+(O operador goto pode ser usado para pular para outra seção do programa.), 
+criando efetivamente uma jumplist e não if / elseif
 */
 
